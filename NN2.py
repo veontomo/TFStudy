07cac80a3e8ccdd6627d70fa3253a721b32b3827
Andrey Shcherbakov <veontomo@gmail.com>
2017-02-18 22:01:34 +0100

    feat: add some casting

--- NN2.py
+++ NN2.py
@@ -1,18 +1,16 @@
 import os
-# import tensorflow as tf
+import tensorflow as tf
 import numpy as np
 
-# logDir = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/") + "/log/"
-# print("Write the log to folder: " + logDir)
-# print("tensorflow version: " + tf.__version__)
+logDir = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/") + "/log/"
+print("Write the log to folder: " + logDir)
+print("tensorflow version: " + tf.__version__)
 L = 3 # dimension of the input vector
 M = 15 # the number of the input vectors
 
-trX = [[0,2,0], [0,2,1]]
-trY = [1,0]
+# trX = [[0,2,0], [0,2,1]]
+# trY = [1,0]
 data = list(range(M))
-
-
 
 def base(n, b):
 	r = n // b
@@ -40,32 +38,29 @@ def isEven(n):
 data2 = list(map(base3, data))
 dataX = list(map(padding4, data2))
 dataY = list(map(isEven, data))
-for i in data:
-	print(i, dataX[i], dataY[i])
 
-exit()
-# X = tf.placeholder("int8", name="inputX") # create symbolic variables
-# Y = tf.placeholder("int8", name="inputY")
+X = tf.placeholder("int8", name="inputX") # create symbolic variables
+Y = tf.placeholder("int8", name="inputY")
 
-# def model(X, w):
-    # return tf. # lr is just X*w so this model line is pretty simple
+def model(X, w):
+	return tf.matmul(tf.cast(X, tf.float64), w) # lr is just X*w so this model line is pretty simple
 
 w = tf.Variable(np.random.rand(1, L), name='weight')
  # w = tf.Variable(0.0, name="weights") # create a shared variable (like theano.shared) for the weight matrix
-# y_model = model(X, w)
+y_model = model(X, w)
 
-# cost = tf.square(Y - y_model) # use square error for cost function
+cost = tf.square(tf.cast(Y, tf.float64) - y_model) # use square error for cost function
 
-# train_op = tf.train.GradientDescentOptimizer(0.01).minimize(cost) # construct an optimizer to minimize cost and fit line to my data
+train_op = tf.train.GradientDescentOptimizer(0.01).minimize(cost) # construct an optimizer to minimize cost and fit line to my data
 
 # Launch the graph in a session
 with tf.Session() as sess:
     # you need to initialize variables (in this case just variable W)
     tf.global_variables_initializer().run()
 
-    for i in range(1):
-        for (x, y) in zip(trX, trY):
-            # sess.run(train_op, feed_dict={X: x, Y: y})
+    for i in range(5):
+        for (x, y) in zip(dataX, dataY):
+            sess.run(train_op, feed_dict={X: x, Y: y})
             print(sess.run(w))
             print("x=", x, "y=", y)
 
