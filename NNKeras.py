e9b22206bff4f86c09c8f05d5413116e830977d4
Andrey Shcherbakov <veontomo@gmail.com>
2017-02-22 09:54:22 +0100

    Adjust the NN architecture to accept the input data

--- NNKeras.py
+++ NNKeras.py
@@ -8,7 +8,7 @@ import numpy as np
 
 # Prepare the data
 
-L = 3                    # dimension of the input vector
+L = 4                    # dimension of the input vector
 M = int(math.pow(3, L))  # the number of available input data (training data + cross-check data + verification data)
 N = 8*M // 10            # size of the training set
 
@@ -18,24 +18,23 @@ print("Parameters:\nL = ", L, " - input vector dimension\nM = ", M, " - size of 
 inputIntegers = list(range(M))
 random.shuffle(inputIntegers)
 
-def base(n, b):
-	"""Base-b representation of number n"""
+def base(n):
+	"""Base-3 representation of number n"""
+	b = 2 # define the base
 	r = n // b
 	if r == 0:
 		return [n]
 	else: 
-		rest = base(r, b)
+		rest = base(r)
 		rest.append(n % b)
 		return rest
 
-def base3(n):
-	return base(n, 3)
 
 def padding(l):
 	"""Pads given list to have a size L"""
 	return (L*[0] + l)[-L:]
 
-def isEven(n):
+def getLabel(n):
 	"""1, if n is even, 0 otherwise"""
 	if (n % 2 == 0):
 	    return 1
@@ -43,38 +42,28 @@ def isEven(n):
 		return 0
 
 
-dataBase3 = list(map(base3, inputIntegers))
-dataX = list(map(padding, dataBase3))
-dataY = list(map(isEven, inputIntegers))
+dataBase = list(map(base, inputIntegers))
+dataX = list(map(padding, dataBase))
+dataY = list(map(getLabel, inputIntegers))
 
-print("X", dataX)
-print("Y", dataY)
+# for i in range(N):
+	# print(inputIntegers[i], " X =", dataX[i], " Y =", dataY[i])
 
 # extract the first elements in order to use tham as a trainig set
 X_train = dataX[:N]
 Y_train = dataY[:N]
 
 
-data = np.random.random((1000, 784))
-labels = np.random.randint(2, size=(1000, 1))
-
 model = Sequential()
-model.add(Dense(1, input_dim=784, activation='sigmoid'))
+model.add(Dense(1, input_dim=L, activation='softmax'))
+# model.add(Dense(2, activation='sigmoid'))
+# model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer='rmsprop',
               loss='binary_crossentropy',
               metrics=['accuracy'])
-# train the model, iterating on the data in batches
-# of 32 samples
-model.fit(data, labels, nb_epoch=10, batch_size=32)
 
-exit()
-# https://keras.io/getting-started/sequential-model-guide/
+model.fit(dataX, dataY, nb_epoch=100, batch_size=32)
 
-model = Sequential()
-model.add(Dense(1, input_dim=L, activation='sigmoid'))
-# model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-model.compile(loss='categorical_crossentropy', optimizer=SGD(lr=0.01, momentum=0.9, nesterov=True))
-model.fit(X_train, Y_train, nb_epoch=5, batch_size=32)
-loss_and_metrics = model.evaluate(X_test, Y_test, batch_size=32)
-classes = model.predict_classes(X_test, batch_size=32)
-proba = model.predict_proba(X_test, batch_size=32)
+for layer in model.layers:
+	print(layer.get_weights())
+
