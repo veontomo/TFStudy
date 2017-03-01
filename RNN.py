d717d462a340f1082666e9eb369ae41cd53edd43
Andrey Shcherbakov <veontomo@gmail.com>
2017-03-01 14:37:47 +0100

    Create a NN with 2 hidden layers

--- RNN.py
+++ RNN.py
@@ -13,12 +13,21 @@ os.system('cls')
 LOW = 0
 HIGH = 127
 L = 8
-E = 20
-TRAINING_SIZE = 60
-X1 = list(range(LOW, HIGH + 1))
-random.shuffle(X1)
-X2 = list(range(LOW, HIGH + 1))
-random.shuffle(X2)
+E = 5000
+TRAINING_SIZE = 100
+TOTAL_SIZE = 128
+raw = list(range(LOW, HIGH + 1))
+s = len(raw)
+X1 = []
+X2 = []
+for i in range(0, TOTAL_SIZE):
+	X1.append(raw[i * 7 % s ])
+	X2.append(raw[i * 11 % s ])
+print(X1, X2)
+# X1 = list(range(LOW, HIGH + 1))
+# random.shuffle(X1)
+# X2 = list(range(LOW, HIGH + 1))
+# random.shuffle(X2)
 Y = [x1 + x2 for x1, x2 in zip(X1, X2)]
 
 
@@ -47,30 +56,28 @@ for (x1, x2) in zip(X1base, X2base):
 
 X = np.array(X).reshape((-1, L, 2))
 Y = np.array(Ybase).reshape((-1, L, 1))
-print(X)
-print(Y)
 
 
 # build the model: 2 stacked LSTM
 print('Build model...')
 model = Sequential()
-model.add(GRU(output_dim = 1, input_length = L, input_dim = 2, return_sequences=True))
-model.add(Activation('relu'))
-#model.add(TimeDistributed(Dense(2, activation='sigmoid')))
-model.add(TimeDistributed(Dense(1, activation='relu')))
+model.add(GRU(output_dim = 5, input_length = L, input_dim = 2, return_sequences = True, activation = 'tanh'))
+model.add(TimeDistributed(Dense(2, activation='sigmoid')))
+model.add(Dense(1, activation='sigmoid'))
+# model.add(TimeDistributed(Dense(1, activation='sigmoid')))
 model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['fbeta_score']) 
 history = model.fit(X[:TRAINING_SIZE], Y[:TRAINING_SIZE], nb_epoch=E, batch_size=8, verbose= 0)
 model.summary()
 
 
 
-print(model.predict(X[TRAINING_SIZE:]))
+print('prediction', X[TRAINING_SIZE:], model.predict(X[TRAINING_SIZE:]))
 
 
 plt.plot(list(range(1, E+1)), history.history['fbeta_score'], 'k', color='green')
 plt.plot(list(range(1, E+1)), history.history['loss'], 'k', color='blue')
 plt.xlabel('Epoch')
-plt.title('Adding bits')
+plt.title('Summing bits')
 
 loss_line = mlines.Line2D([], [], color='blue', label='loss')
 fscore_line = mlines.Line2D([], [], color='green', label='F1 score')
