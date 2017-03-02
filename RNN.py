8f76aa65a49b87a7e2a1b9f52b8790266c05b631
Andrey Shcherbakov <veontomo@gmail.com>
2017-03-02 10:08:49 +0100

    Display layers' input/output shapes

--- RNN.py
+++ RNN.py
@@ -1,32 +1,33 @@
 import os
 from keras.models import Sequential
 from keras.layers import Dense, GRU, Activation, LSTM, TimeDistributed
-from keras.optimizers import SGD
-import random
-import math 
+#from keras.optimizers import SGD
+#import random
+#import math 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
 from keras.utils.visualize_util import plot
 
+dirName = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
 
 os.system('cls')
 
 LOW = 0
 HIGH = 127
 L = 8
-E = 50
-TRAINING_SIZE = 100
-TOTAL_SIZE = 128
+E = 2
+NUM_TRAIN_INPUTS = 100
+NUM_TOTAL_INPUTS = 128
 raw = list(range(LOW, HIGH + 1))
 s = len(raw)
 X1 = []
 X2 = []
-for i in range(0, TOTAL_SIZE):
+for i in range(0, NUM_TOTAL_INPUTS):
 	X1.append(raw[i * 7 % s ])
 	X2.append(raw[i * 11 % s ])
-print(X1, X2)
+
 # X1 = list(range(LOW, HIGH + 1))
 # random.shuffle(X1)
 # X2 = list(range(LOW, HIGH + 1))
@@ -61,23 +62,24 @@ X = np.array(X).reshape((-1, L, 2))
 Y = np.array(Ybase).reshape((-1, L, 1))
 
 
-# build the model: 2 stacked LSTM
 print('Build model...')
 model = Sequential()
-model.add(GRU(output_dim = L, input_length = L, input_dim = 2, return_sequences = True, activation = 'tanh'))
+model.add(LSTM(output_dim = 2*L, input_length = L, input_dim = 2, return_sequences = True, activation = 'tanh', name="analyzer"))
 model.add(Dense(L, activation='sigmoid'))
 model.add(TimeDistributed(Dense(2, activation='sigmoid')))
 model.add(Dense(1, activation='sigmoid'))
-# model.add(TimeDistributed(Dense(1, activation='sigmoid')))
+model.add(TimeDistributed(Dense(1, activation='sigmoid')))
 model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['fbeta_score']) 
-# plot(model, to_file='model.png')
-history = model.fit(X[:TRAINING_SIZE], Y[:TRAINING_SIZE], nb_epoch=E, batch_size=8, verbose= 0)
+plot(model, to_file= dirName + '/model_diagram.png')
+history = model.fit(X[:NUM_TRAIN_INPUTS], Y[:NUM_TRAIN_INPUTS], nb_epoch=E, batch_size=8, verbose= 0)
 model.summary()
 
 
 
-print('prediction', X[TRAINING_SIZE:], model.predict(X[TRAINING_SIZE:]))
+# print('prediction', X[NUM_TRAIN_INPUTS:], model.predict(X[NUM_TRAIN_INPUTS:]))
 
+for layer in model.layers:
+	print(layer.name, 'input shape', layer.input_shape, 'output shape', layer.output_shape)
 
 plt.plot(list(range(1, E+1)), history.history['fbeta_score'], 'k', color='green')
 plt.plot(list(range(1, E+1)), history.history['loss'], 'k', color='blue')
