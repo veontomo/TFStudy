2362245bfcdb58a3d0e8223f19abd0295be1545e
Andrey Shcherbakov <veontomo@gmail.com>
2017-06-15 08:37:45 +0200

    Digitalize the data set

--- Titanic/script.py
+++ Titanic/script.py
@@ -1,19 +1,57 @@
 #import os
 #import tensorflow as tf
+import re
+
+# Read the data into a list
 with open("train.csv", encoding="ascii") as file:
     list = file.readlines()
+
+# First element of the list contains the feature names
 features = list[0].strip().split(",")
+
+# features that should be treated as integers
+intFeatures = ["PassengerId", "Survived", "Pclass", "SibSp", "Parch"]
+# features that should be treated as floats
+floatFeatures = ["Age", "Fare"]
+
 feature_values = [[] for i in range(0, len(features)) ]
+
+# iterate over the data set in order to collect all possible feature values
 for i in range(1, len(list)):
-    line=list[i].strip().split(",")
-    for k in range(0, len(line)-1):
-        if features[k] in ["Survived", "Pclass", "Sex"]:
+    line = re.split(",(?!\s)", list[i].strip())
+    for k in range(0, len(line)):
+        if features[k] in intFeatures:
             value = int(line[k])
+        elif features[k] in floatFeatures:
+            if line[k] != '':
+                value = float(line[k])
+            else:
+                value = -1
         else:
             value = line[k]
         if not(value in feature_values[k]):
             feature_values[k].append(value)
+            feature_values[k].sort()
 
 
-print(feature_values)
-print(len(list))
+# indexed version of the data set (in order to avoid strings) 
+digitalized = []
+
+for i in range(1, len(list)):
+    tmpLine = []
+    line = re.split(",(?!\s)", list[i].strip())
+    for k in range(0, len(line)):
+        if features[k] in intFeatures:
+            value = int(line[k])
+        elif features[k] in floatFeatures:
+            if line[k] != '':
+                value = float(line[k])
+            else:
+                value = -1
+        else:
+            value = line[k]
+        tmpLine.append(feature_values[k].index(value))
+    digitalized.append(tmpLine)
+
+
+print(digitalized)
