a41f50f3b26defc30aa652aff6358e6c907bae8b
Andrey Shcherbakov <veontomo@gmail.com>
2017-06-15 06:33:52 +0200

    Start digitalization of the feature values

--- Titanic/script.py
+++ Titanic/script.py
@@ -7,8 +7,12 @@ feature_values = [[] for i in range(0, len(features)) ]
 for i in range(1, len(list)):
     line=list[i].strip().split(",")
     for k in range(0, len(line)-1):
-        if not(line[k] in feature_values[k]):
-            feature_values[k].append(line[k])
+        if features[k] in ["Survived", "Pclass", "Sex"]:
+            value = int(line[k])
+        else:
+            value = line[k]
+        if not(value in feature_values[k]):
+            feature_values[k].append(value)
 
 
 print(feature_values)
