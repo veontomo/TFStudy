7dceeaa365cab46617f8c9c0f9247d3e60bc09c5
Andrey Shcherbakov <veontomo@gmail.com>
2017-06-17 08:47:31 +0200

    Construct new features Deck and CabinNumber from Cabin

--- Titanic/script.py
+++ Titanic/script.py
@@ -8,27 +8,58 @@ from keras.optimizers import SGD
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
+def cabin(str):
+    """Split the cabin number into a string and an integer"""
+    if str == "":
+        return ["", 0]
+    regex1 = "([A-G\s]+)(\d+)"
+    if re.match(regex1, str):
+        parts = re.search(regex1, str)
+        part1 = parts.group(1)
+        part2 = parts.group(2)
+        return [part1,  int(part2)]
+    regex2 = "([A-GT\s]+)"
+    if re.match(regex2, str):
+        parts = re.search(regex2, str)
+        part1 = parts.group(1)
+        return [part1, 0]
+    regex3 = "(\d+)"
+    if re.match(regex3, str):
+        parts = re.search(regex3, str)
+        part1 = parts.group(1)
+        return ["", int(part1)]
+    return ["", 0]
+
 # Read the data into a list
 with open("train.csv", encoding="ascii") as file:
     list = file.readlines()
 
 # First element of the list contains the feature names
-features = list[0].strip().split(",")
+orig_features = list[0].strip().split(",")
+generated_features = orig_features[:]
+generated_features.append("Deck")
+generated_features.append("CabinNumber")
 
 # features that should be treated as integers
 intFeatures = ["PassengerId", "Survived", "Pclass", "SibSp", "Parch"]
 # features that should be treated as floats
 floatFeatures = ["Age", "Fare"]
 
-feature_values = {key: [] for key in features }
-
-
+feature_values = {key: [] for key in generated_features}
 
 # iterate over the data set in order to collect all possible feature values
 for i in range(1, len(list)):
     line = re.split(",(?!\s)", list[i].strip())
     for k in range(0, len(line)):
-        featureName = features[k]
+        featureName = orig_features[k]
+        if featureName == "Cabin":
+            values = cabin(line[k])
+            if not(values[0] in feature_values["Deck"]):
+                feature_values["Deck"].append(values[0])
+                feature_values["Deck"].sort()
+            if not(values[1] in feature_values["CabinNumber"]):
+                feature_values["CabinNumber"].append(values[1])
+                feature_values["CabinNumber"].sort()
         if featureName in intFeatures:
             value = int(line[k])
         elif featureName in floatFeatures:
@@ -42,41 +73,42 @@ for i in range(1, len(list)):
             feature_values[featureName].append(value)
             feature_values[featureName].sort()
 
-print(feature_values)
-exit()
-
-# indexed version of the data set (in order to avoid strings) 
+# indexed version of the data set (in order to avoid strings)
 digitalized = []
 
 for i in range(1, len(list)):
     tmpLine = []
+    tmpCabin = []
     line = re.split(",(?!\s)", list[i].strip())
     for k in range(0, len(line)):
-        if features[k] in intFeatures:
+        if orig_features[k] in intFeatures:
             value = int(line[k])
-        elif features[k] in floatFeatures:
+        elif orig_features[k] in floatFeatures:
             if line[k] != '':
                 value = float(line[k])
             else:
                 value = -1
         else:
             value = line[k]
-        tmpLine.append(feature_values[k].index(value))
+        tmpLine.append(feature_values[orig_features[k]].index(value))
+        if orig_features[k] == "Cabin":
+            values = cabin(line[k])
+            tmpCabin.append(feature_values["Deck"].index(values[0]))
+            tmpCabin.append(feature_values["CabinNumber"].index(values[1]))
+    tmpLine.append(tmpCabin[0])
+    tmpLine.append(tmpCabin[1])
     digitalized.append(tmpLine)
 
-
-pos = [features.index("Pclass"), features.index("Sex"), features.index("Age"), features.index("SibSp"), features.index("Parch"), features.index("Fare"), features.index("Cabin"), features.index("Embarked")]
-
+#pos = [orig_features.index("Pclass"), orig_features.index("Sex"), orig_features.index("Age"), orig_features.index("SibSp"), orig_features.index("Parch"), orig_features.index("Fare"), orig_features.index("Cabin"), orig_features.index("Embarked")]
+pos = [generated_features.index(key) for key in ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "Deck", "CabinNumber"]]
 # input data
 dataX = np.array(digitalized)[:, pos]
 # labels
-dataY = np.array(digitalized)[:, features.index("Survived")]
-
-
+dataY = np.array(digitalized)[:, generated_features.index("Survived")]
 
 L = len(pos)
 N = int(0.8 * len(dataY))
-E = 200
+E = 500
 
 X_train = dataX[:N]
 Y_train = dataY[:N]
