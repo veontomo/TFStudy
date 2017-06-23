c536c463d2bf25fd1c7d043dac00e64758c3cab7
Andrey Shcherbakov <veontomo@gmail.com>
2017-06-23 22:15:01 +0200

    Use train data set (with a sale price)

--- House/script.py
+++ House/script.py
@@ -1,9 +1,12 @@
 import csv
 import operator
 import re
+import numpy as np
+from sklearn import ensemble
+
 
 # Read the data into a list
-with open("test.csv", encoding="ascii") as file:
+with open("train.csv", encoding="ascii") as file:
     list = file.readlines()
 
 title = [v.strip() for v in list[0].split(",")]
@@ -20,7 +23,7 @@ intFieldNamesAsString = "Id, MSSubClass, LotFrontage, LotArea, OverallQual, Over
     MasVnrArea, BsmtFinSF1, BsmtFinSF2, BsmtUnfSF, TotalBsmtSF, 1stFlrSF, 2ndFlrSF, LowQualFinSF, GrLivArea, \
     BsmtFullBath, BsmtHalfBath, FullBath, HalfBath, BedroomAbvGr, KitchenAbvGr, TotRmsAbvGrd, Fireplaces, \
     GarageYrBlt, GarageCars, GarageArea, WoodDeckSF, OpenPorchSF, EnclosedPorch, 3SsnPorch, ScreenPorch, \
-    PoolArea, MiscVal, MoSold, YrSold"
+    PoolArea, MiscVal, MoSold, YrSold, SalePrice"
 intFieldNames = re.split(",\s*", intFieldNamesAsString)
 intFieldPositions = [title.index(k) for k in intFieldNames]
 
@@ -31,7 +34,7 @@ strFieldNamesAsString = "MSZoning, Street, Alley, LotShape, LandContour, \
     MasVnrType, ExterQual, ExterCond, Foundation, BsmtQual, BsmtCond, \
     BsmtExposure, BsmtFinType1, BsmtFinType2, Heating, HeatingQC, CentralAir, \
     Electrical, Functional, FireplaceQu, GarageType, GarageFinish, GarageQual, \
-    GarageCond, PavedDrive, PoolQC, Fence, MiscFeature, SaleType, SaleCondition"
+    GarageCond, PavedDrive, PoolQC, Fence, MiscFeature, SaleType, KitchenQual, SaleCondition"
 strFieldNames = re.split(",\s*", strFieldNamesAsString)
 strFieldPositions = [title.index(k) for k in strFieldNames]
 
@@ -79,25 +82,98 @@ def digitalize(featureNames, featureValues, featureIndex, indexedPositions):
 
 
 dataIndex = index(title, valuesCast)
+
 digitalized = digitalize(title, valuesCast, dataIndex, strFieldPositions )
-print(digitalized)
+dataX = np.array(digitalized)[:, :-1]
+# labels
+dataY = np.array(digitalized)[:, -1]
 
-exit()
-frequency = {}
-dataSize = len(values)
-for key in dataIndex:
-    l = len(dataIndex[key])
-    p = int(l/dataSize * 10000) / 100
-    frequency[key] = p
 
-frequency_sorted = sorted(frequency.items(), key=operator.itemgetter(1))
-threshold = 0.0
+F = len(dataX[0])
+T = int(0.8 * len(dataX))
+E = 200
 
-for key in frequency_sorted:
-    if key[0] == 'Id':
-        continue
-    if key[1] > threshold:
-        print(key[0], key[1], dataIndex[key[0]], '\n')
+print("number of features", F)
+print("number of training data", T)
+print("number of epochs", E)
 
-for k, v in zip(title, values[5]):
-    print(k, v)
+X_train = dataX[:T]
+Y_train = dataY[:T]
+
+X_cv = dataX[T:]
+Y_cv = dataY[T:]
+
+CV = len(X_cv)
+print("number of cross validation", CV)
+
+# frequency = {}
+# dataSize = len(values)
+# for key in dataIndex:
+#     l = len(dataIndex[key])
+#     p = int(l/dataSize * 10000) / 100
+#     frequency[key] = p
+#
+# frequency_sorted = sorted(frequency.items(), key=operator.itemgetter(1))
+# threshold = 0.0
+#
+# for key in frequency_sorted:
+#     if key[0] == 'Id':
+#         continue
+#     if key[1] > threshold:
+#         print(key[0], key[1], dataIndex[key[0]], '\n')
+#
+# for k, v in zip(title, values[5]):
+#     print(k, v)
+
+def F1Score(actual, prediction):
+    if len(actual) != len(prediction):
+        print("Predicted and actual lists must be of equal length")
+        return -1
+    # Calculate F score on the cross-validation data
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+    for (pred, act) in zip(prediction, actual):
+        if pred == 1:
+            if act == 1:
+                tp += 1
+            else:
+                fp += 1
+        else:
+            if act == 1:
+                fn += 1
+            else:
+                tn += 1
+
+    #print("true positive:", tp, "\ntrue negative:", tn, "\nfalse negative:", fn, "\nfalse positive:", fp)
+    if tp + tn + fp + fn != len(actual):
+        print('Partition to true/false positive/negative went wrong')
+        return -1
+    #print(tp, "+", tn, "+", fn, "+", fp, "=" if tp + fp + fn + tn == len(X_test) else "<>", len(X_test))
+
+    denom1 = tp + fp
+    denom2 = tp + fn
+    if denom1!= 0:
+        precision = tp / denom1
+     #   print("precision:", precision)
+    else:
+        precision = -1
+    if denom2 != 0:
+        recall = tp / denom2
+    #    print("recall:", recall)
+    else:
+        recall = -1
+    if (recall != -1) and (precision != -1):
+        Fscore = 2 * precision * recall / (precision + recall)
+    else:
+        Fscore = -1
+    return Fscore
+
+
+rf = ensemble.RandomForestClassifier(n_estimators=10).fit(X_train, Y_train)
+predictions = rf.predict(X_cv)
+
+Fscore = F1Score(Y_cv, predictions)
+
+print('F1 score =', Fscore)
