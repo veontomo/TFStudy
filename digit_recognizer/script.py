a0f0232f7952747933a9c6bd9678f6b4d25e7e27
Andrey Shcherbakov <veontomo@gmail.com>
2017-07-13 09:38:31 +0200

    Remove lines of code previously used as a try

--- digit_recognizer/script.py
+++ digit_recognizer/script.py
@@ -8,13 +8,6 @@ import matplotlib.lines as mlines
 import math
 
 
-def f(t):
-    return np.exp(-t) * np.cos(2*np.pi*t)
-
-t1 = np.arange(0.0, 5.0, 0.1)
-t2 = np.arange(0.0, 5.0, 0.02)
-
-
 def mod(x):
     return math.fabs(x)
 
