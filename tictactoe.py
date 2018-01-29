de9695588b76bf1582edf6e16ca33423e0b23e7a
Andrey Shcherbakov <veontomo@gmail.com>
2018-01-29 21:38:55 +0100

    First apprx for Q-table the first player move in tic tac toe

--- tictactoe.py
+++ tictactoe.py
@@ -26,10 +26,24 @@ def get_free_positions(state):
     return [i for i, x in enumerate(state) if x == EMPTY]
 
 
+def add_to_dict(dict, state, mark, pos, value):
+    if state not in dict:
+        dict[state] = {}
+    if mark not in dict[state]:
+        dict[state][mark] = {}
+    dict[state][mark][pos] = value
+
+
 def make_move(state, pos, mark):
     chars = list(state)
     chars[pos] = mark
     return "".join(chars)
+
+
+def move_reward(state, pos, mark, size):
+    new_state = make_move(state, pos, mark)
+    r = reward(new_state, pos, size)
+    return new_state, r
 
 
 def reward(state, index, size):
@@ -88,7 +102,7 @@ Q = {}
 state2 = initState
 mark = MARK_X
 EPOCHS = 20
-gamma = 0.99
+gamma = 0.6
 free_positions = get_free_positions(state2)
 # initialize available keys in Q table
 for free_position in free_positions:
@@ -141,33 +155,49 @@ for i in free_positions:
 # so that
 # Q[S][M][P] gives the reward for placing the mark M in cell P when the state is S.
 
-Q = {}
-for first_move in get_free_positions(EMPTY_STATE):
-    if EMPTY_STATE not in Q:
-        Q[EMPTY_STATE] = {}
-    if MARK_X not in Q[EMPTY_STATE]:
-        Q[EMPTY_STATE][MARK_X] = {}
-    Q[EMPTY_STATE][MARK_X][first_move] = random.random()
+Q_X = {}
+positions = get_free_positions(EMPTY_STATE)
+if EMPTY_STATE not in Q_X:
+    Q_X[EMPTY_STATE] = {i: random.random() for i in positions}
+print(Q_X)
+mark = MARK_X
+opponent_mark = toggle_mark(mark)
 
-for state in list(Q.keys()):
-    for mark in Q[state]:
-        for pos in Q[state][mark]:
-            new_state = make_move(state, pos, mark)
-            r = reward(new_state, pos, N)
-            # make all the moves of the other player
-            available_positions = get_free_positions(new_state)
-            opponent_mark = toggle_mark(mark)
-            for opponent_position in available_positions:
-                state_after_opponent_move = make_move(new_state, opponent_position, opponent_mark)
-                opponent_reward = reward(state_after_opponent_move, opponent_position, N)
-                if state_after_opponent_move not in Q:
-                    Q[state_after_opponent_move] = {opponent_mark: dict([(i, random.random()) for i in get_free_positions(state_after_opponent_move)])}
-                if opponent_reward == 1:
-                    Q[state_after_opponent_move][opponent_mark][opponent_position] = opponent_reward
+EPOCHS = 60
+for e in range(0, EPOCHS):
+    print("epoch " + str(e))
+    for state in list(Q_X.keys()):
+        for pos in list(Q_X[state]):
+            s, r = move_reward(state, pos, mark, N)
+            if r == 1:
+                # print("move to position " + str(pos) + " in state " + state + " is a winning one")
+                # print(Q_X[state])
+                Q_X[state][pos] = 1
+                break
+            current_max = 0
+            opponent_positions = get_free_positions(s)
+            for opponent_position in opponent_positions:
+                state_after_opponent_move, opponent_reward = move_reward(s, opponent_position, opponent_mark, N)
+                if state_after_opponent_move not in Q_X:
+                    Q_X[state_after_opponent_move] = {i: random.random() for i in
+                                                      get_free_positions(state_after_opponent_move)}
+                if opponent_reward == 1 and Q_X[state][pos] != -1:
+                    Q_X[state][pos] = -1
+                tmp = max(Q_X[state_after_opponent_move].values())
+                if tmp > current_max:
+                    current_max = tmp
+            Q_X[state][pos] = r + gamma * current_max
+    # print(Q_X[EMPTY_STATE])
 
-
+print(len(Q_X))
+print(Q_X["...xx.oo."])
+print(Q_X["........."])
+exit()
 for state in Q:
+    print("state: " + state)
     for mark in Q[state]:
+        print("mark: " + str(mark))
         for pos in Q[state][mark]:
-            print("state: " + state + ", mark: " + mark + ", position: " + str(pos) + ", reward: " + str(
-                Q[state][mark][pos]))
+            print("pos: " + str(pos) + ", reward: " + str(Q[state][mark][pos]))
+            # + ", position: " + str(pos))
+            # + ", reward: " + str(Q[state][mark][pos]))
